b4f8d8a84bedacfd021c76c9ca5a65d7340d37bb
Fopezyode <akinpelumijoseph7@gmail.com>
2024-11-04 06:12:27 +0100

    Python

---
+++ task.py
@@ -0,0 +1,6 @@
+print ("Welcome to the Band Name Generator")
+
+place = input("What is the name of the City You Grew Up in?\n")
+pet = ("What is the Name of your Pet?\n")
+print("Your band name could be: " + place + " " + pet)
+
